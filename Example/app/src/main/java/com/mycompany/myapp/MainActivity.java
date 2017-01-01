2dec3e795015151595ffca0486589158a27c5457
GTech455 <jeanlucasssantos570@gmail.com>
2017-01-01 02:39:34 -0200

    Updated!!

--- Example/app/src/main/java/com/mycompany/myapp/MainActivity.java
+++ Example/app/src/main/java/com/mycompany/myapp/MainActivity.java
@@ -3,12 +3,16 @@ package com.mycompany.myapp;
 import android.app.*;
 import android.os.*;
 
-public class MainActivity extends Activity 
+public class MainActivity extends ListActivity 
 {
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        String[] lt = {
+        "4455",
+        };
+        setListAdapter(new ArrauAdapter<String>(this, android.R.layout.simple_list_item_1));
     }
 }
